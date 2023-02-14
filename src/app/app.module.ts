d4b6ce6934ff3334daa0cbfb895d45f30cdfe0eb
rjstauffer <rileyjstauffer@gmail.com>
2023-02-14 18:33:05 -0500

    lab2 2/14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './navigation/top-nav-bar/top-nav-bar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    JumbotronComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
