dfd42226930feaea3b2020a49953b126ab0f19b5
rjstauffer <rileyjstauffer@gmail.com>
2023-04-12 18:32:25 -0400

    lab9.1
    Created environments and added code from firebase in attempt to deploy app

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+export const environment = {
+    production: false
+};
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-uSUF0Pb-o8rLjKcyTfZMYyFDOAqw-ic",
+  authDomain: "bricklink-app.firebaseapp.com",
+  databaseURL: "https://bricklink-app-default-rtdb.firebaseio.com",
+  projectId: "bricklink-app",
+  storageBucket: "bricklink-app.appspot.com",
+  messagingSenderId: "204867705399",
+  appId: "1:204867705399:web:8ddaaee76c0d2ccc4efdb2",
+  measurementId: "G-K4V60LWB3B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
