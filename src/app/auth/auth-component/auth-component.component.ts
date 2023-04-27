f90c16573b2fc43140101e3052945708a496959b
rjstauffer <rileyjstauffer@gmail.com>
2023-04-27 14:39:30 -0400

    lab10_1
    goal 4

--- src/app/auth/auth-component/auth-component.component.ts
+++ src/app/auth/auth-component/auth-component.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { AuthResponse } from './authResponse';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-component',
@@ -13,7 +14,7 @@ export class AuthComponentComponent {
   public buttonClicked!:string;
   private authObservable!: Observable<AuthResponse>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router:Router) {
 
   }
 
@@ -35,6 +36,7 @@ export class AuthComponentComponent {
     this.authObservable.subscribe(
       (data: AuthResponse) => {
         console.log(data);
+        this.router.navigate(['']);
       },
       error => {
         console.log(error.error.error.message);
--- src/app/auth/auth-component/auth.service.ts
+++ src/app/auth/auth-component/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { AuthResponse } from "./authResponse";
+import { Subject, tap } from "rxjs";
+import { User } from "./user.model";
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +13,7 @@ export class AuthService {
     baseUrl:string = "https://identitytoolkit.googleapis.com/v1/accounts";
     signUpEndpoint:string = "signUp";
     signInEndpoint:string = "signInWithPassword";
+    private user:Subject<User> = new Subject<User>();
 
     public constructor(private http: HttpClient) {
 
@@ -26,15 +29,24 @@ export class AuthService {
 
     }
 
-    public login(email:string, password: string)
-    {
+    public login(email: string, password: string) {
         const requestBody = {
-            "email":email,
-            "password":password,
+            "email": email,
+            "password": password,
             "returnSecureToken": true
         };
 
-        return this.http.post<AuthResponse>(this.baseUrl+':'+this.signInEndpoint+'?'+'key='+environment.firebase.apiKey , requestBody);
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signInEndpoint +
+            '?' + 'key=' + environment.firebase.apiKey, requestBody).pipe(
+                tap(
+                    (data: AuthResponse) => {
+                        const currentTime = new Date().getTime();
+                        const expirationDate = new Date(currentTime + +data.expiresIn * 1000);
+                        const user = new User(data.email, data.localId, data.idToken, expirationDate);
+                        this.user.next(user);
+                    }
+                )
+            );
     }
 
 }
